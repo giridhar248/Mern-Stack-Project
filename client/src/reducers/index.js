e4e5ce72347a0f89b63f5500e871d04ed9029659
giridhar248 <88219987+giridhar248@users.noreply.github.com>
2023-09-03 22:45:11 +0530

    Delete client directory

--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import stories from "./stories";
-import authentication from "./authentication";
-
-export default combineReducers({
-    stories,
-    authentication
-});
